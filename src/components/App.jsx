4ea43e123bb973195c31709b01acf2fd9a452ef8
steve2116 <s.ackers@hotmail.co.uk>
2023-05-30 11:22:52 +0100

    Initial setup

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import Main from "./Main";
+
+export default function App() {
+    return (
+        <>
+            <main>
+                <Main />
+            </main>
+        </>
+    );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+export default function Main() {
+    return (
+        <>
+            <Routes>
+                <Route
+                    to=""
+                    element={""}
+                />
+            </Routes>
+        </>
+    );
+}
