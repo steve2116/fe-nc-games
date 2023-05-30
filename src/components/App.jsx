4bc69bca59f24e7b0401657aa0d261a86bf30bff
steve2116 <s.ackers@hotmail.co.uk>
2023-05-30 12:28:13 +0100

    adding styling and homepage components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+
+import Navbar from "./Navbar.jsx";
+import Categories from "./Categories";
 import Main from "./Main.jsx";
 import "../designs/App.css";
 
 export default function App() {
+    const [hoverClass, setHoverClass] = useState("content-container");
+
     return (
         <>
-            <main>
-                <Main />
-            </main>
+            <nav>
+                <Navbar />
+            </nav>
+            <div className={hoverClass}>
+                <div
+                    onClick={() => {
+                        setHoverClass("hover-cats");
+                    }}
+                    onMouseLeave={() => {
+                        setHoverClass("content-container");
+                    }}
+                    id="category-menu"
+                >
+                    <Categories />
+                </div>
+                <main>
+                    <Main />
+                </main>
+            </div>
+            <footer>PLACEHOLDER FOOTER</footer>
         </>
     );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,21 @@
 import { Routes, Route } from "react-router-dom";
+import { useContext } from "react";
 
+import { userContext } from "../contexts/user";
+import Home from "./Home.jsx";
 import ReviewList from "./ReviewList.jsx";
 
 export default function Main() {
+    const { user, setUser } = useContext(userContext);
+
     return (
         <>
+            <p>Currently logged in as: {user.username}</p>
             <Routes>
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
                 <Route
                     path="/reviews"
                     element={<ReviewList />}
