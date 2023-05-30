43feec97d10d93c0269494b25a7abce39f403791
steve2116 <s.ackers@hotmail.co.uk>
2023-05-30 11:27:43 +0100

    initial setup

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ export default function Main() {
         <>
             <Routes>
                 <Route
-                    to=""
+                    to="/"
                     element={""}
                 />
             </Routes>
